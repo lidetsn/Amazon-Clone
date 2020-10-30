de6832d469178ac905bd3667227536d72b6d81e0
unknown <luk4ls@yahoo.com>
2020-10-30 00:34:08 -0700

    home page added

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -1,12 +1,18 @@
+import  React from "react"
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom"
+import Home from "./pages/Home"
+import Header from "./components/Header"
 
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-       
-      </header>
+    <div>
+        <Router>
+          <Header/>
+          <Switch>
+            <Route path="/" component={Home}/>
+          </Switch>
+        </Router>
     </div>
   );
 }
